aaa0927153efb2bed0016f50f4045721f59c53a5
924172407 <mehul.anskey@gmail.com>
2023-12-12 16:40:22 +0530

    initial comit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello I'm Mitesh Patel</h2>
+    </div>
+  );
+}
+
+export default App;
